a2f37a4c3de2c636c9d7c2d6cb6b4fb717a083b2
SelenEris <40161882+SelenEris@users.noreply.github.com>
2018-11-27 10:35:30 +0100

    squelette classe signature

--- src/SignatureTools.java
+++ src/SignatureTools.java
@@ -1,2 +1,13 @@
 public class SignatureTools {
+    public SignatureTools(String file, char[] passwd, String type, String DN) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean verify(String fileName, byte[] signature) {
+        return true;
+    }
 }
